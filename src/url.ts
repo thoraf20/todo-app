c110ba150784ae72eb881f67edaa3d7cd2c34ce7
Toheeb Rauf <thoraf20@gmail.com>
2022-11-11 21:59:38 +0100

    all items in a todo route

--- src/url.ts
+++ src/url.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { addIemHandler, createTodoHandler, duplicateTodoHandler, fetchTodosHandler, getAllItemsInTodoHandler, updateIemHandler } from "./routes/todo";
+import { addIemHandler, createTodoHandler, duplicateTodoHandler, fetchTodosHandler, getAllItemsInTodoHandler, getATodoHandler, updateIemHandler } from "./routes/todo";
 
 
 const router = express.Router();
 
 router.get("/todo/_all", fetchTodosHandler);
 router.get("/todo/list_items", getAllItemsInTodoHandler);
+router.get("/todo/_single", getATodoHandler);
 
 router.post("/todo/_create", createTodoHandler);
 router.post("/todo/add_item", addIemHandler);
