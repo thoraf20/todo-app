2724ce33560acde5818403926d3ced9f63653cc5
Toheeb Rauf <thoraf20@gmail.com>
2022-11-13 16:46:05 +0100

    add unit tests

--- src/test/todo.test.ts
+++ src/test/todo.test.ts
@@ -21,6 +21,59 @@ describe('TodoService', () => {
       itemDescription: 'Testing the todo route',
       status: Status.NOT_STARTED
     }])
+    TodoModel.find = jest
+      .fn()
+      .mockResolvedValue({ 
+        _id: '6370f8c8c8b6ba143edb477f',
+        name: 'New List',
+        description: 'Testing the todo route',
+        items: [{ 
+          _id: '6370f8c8c8b6ba143edb477f',
+          itemName: 'New List',
+          itemDescription: 'Testing the todo route',
+          status: Status.NOT_STARTED
+        },
+        {
+          id: '6370f8c8c8b6ba143edb477f',
+          itemName: 'New List',
+          itemDescription: 'Testing the todo route',
+          status: Status.NOT_STARTED
+        }]
+      })
+      TodoModel.findById = jest
+      .fn()
+      .mockResolvedValue({ 
+        _id: '6370f8c8c8b6ba143edb477f',
+        name: 'New List',
+        description: 'Testing the todo route',
+        items: [{ 
+          _id: '6370f8c8c8b6ba143edb4787',
+          itemName: 'New List',
+          itemDescription: 'Testing the todo route',
+          status: Status.NOT_STARTED
+        }]
+      })
+
+      TodoModel.updateOne = jest
+      .fn()
+      .mockResolvedValue({ 
+        _id: '6370f8c8c8b6ba143edb477f',
+        name: 'New List',
+        description: 'Testing the todo route',
+        items: [{ 
+          _id: '6370f8c8c8b6ba143edb4787',
+          itemName: 'New List',
+          itemDescription: 'Testing the todo route',
+          status: Status.NOT_STARTED
+        }]
+      })
+
+      TodoModel.findByIdAndRemove = jest
+      .fn()
+      .mockResolvedValue({ 
+        status: true,
+        message: 'List Deleted Successfully',
+      })
   });
 
     it('should be defined', () => {
@@ -57,6 +110,85 @@ describe('TodoService', () => {
           itemDescription: 'Testing the todo route',
           status: Status.NOT_STARTED
         }])
+      })
+    })
+
+    describe('fetctAll Todo List', () => {
+      it('it should return output given the correct input', async () => {
+
+        const fetchAllTodos = await TodoService.fetchAllTodos()
+
+        expect(fetchAllTodos).toEqual({ 
+          _id: '6370f8c8c8b6ba143edb477f',
+          name: 'New List',
+          description: 'Testing the todo route',
+          items: [{ 
+            _id: '6370f8c8c8b6ba143edb477f',
+            itemName: 'New List',
+            itemDescription: 'Testing the todo route',
+            status: Status.NOT_STARTED
+          },
+          {
+            id: '6370f8c8c8b6ba143edb477f',
+            itemName: 'New List',
+            itemDescription: 'Testing the todo route',
+            status: Status.NOT_STARTED
+          }]
+        })
+      })
+    })
+
+    describe('fetct A single', () => {
+      it('it should return output given the correct input', async () => {
+
+        const fetchATodo = await TodoService.fetchATodo('6370f8c8c8b6ba143edb477f')
+
+        expect(fetchATodo).toEqual({ 
+          _id: '6370f8c8c8b6ba143edb477f',
+          name: 'New List',
+          description: 'Testing the todo route',
+          items: [{ 
+            _id: '6370f8c8c8b6ba143edb4787',
+            itemName: 'New List',
+            itemDescription: 'Testing the todo route',
+            status: Status.NOT_STARTED
+          }]
+        })
+      })
+    })
+    
+    describe('update List', () => {
+      it('it should return output given the correct input', async () => {
+
+        const updateList = await TodoService.updateList({
+          _id: '6370f8c8c8b6ba143edb477f',
+          name: 'New List',
+          description: 'New Update List'
+        })
+
+        expect(updateList).toEqual({ 
+          _id: '6370f8c8c8b6ba143edb477f',
+          name: 'New List',
+          description: 'Testing the todo route',
+          items: [{ 
+            _id: '6370f8c8c8b6ba143edb4787',
+            itemName: 'New List',
+            itemDescription: 'Testing the todo route',
+            status: Status.NOT_STARTED
+          }]
+        })
+      })
+    })
+
+    describe('update List', () => {
+      it('it should return output given the correct input', async () => {
+
+        const updateList = await TodoService.deleteTodo('6370f8c8c8b6ba143edb477f',)
+
+        expect(updateList).toEqual({ 
+          status: true,
+          message: 'List Deleted Successfully',
+        })
       })
     })
   }
